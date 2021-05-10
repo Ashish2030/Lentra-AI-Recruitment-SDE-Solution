1ba9febc89f968c16c493db0a7118d0451fcabc0
Ashish Kumar <kumarashish79924@gmail.com>
2021-05-10 20:17:58 +0530

    𝐅𝐢𝐧𝐚𝐥 𝐏𝐫𝐨𝐣𝐞𝐜𝐭

---
+++ 𝐎𝐝𝐝_𝐨𝐫_𝐇𝐚𝐥𝐟_𝐂𝐞𝐧𝐭𝐮𝐫𝐲.java
@@ -0,0 +1,56 @@
+/* Save this in a file called Main.java to compile and test it */
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static int processArray(ArrayList<Integer> array) {
+        /* 
+         * Do not make any changes outside this method.
+         *
+         * Modify this method to process `array` as indicated
+         * in the question. At the end, return the size of the
+         * array. 
+         *
+         * Do not print anything in this method
+         *
+         * Submit this entire program (not just this function)
+         * as your answer
+         */
+         for(int i=0;i<array.size();i++)
+         {
+             if(array.get(i)%2==0&&array.get(i)>50)
+             {
+                 array.set(i,-3);
+             }
+             else if((array.get(i)%2!=0)&&(array.get(i)>50))
+             {
+               array.set(i,-2);  
+             }
+             else if(array.get(i)%2!=0)
+             {
+                 array.set(i,-1);
+             }
+              else
+            {
+             
+            }
+         }
+        
+        return array.size();
+    }
+
+    public static void main (String[] args) {
+        ArrayList<Integer> arrayList = new ArrayList<Integer>();
+        Scanner in = new Scanner(System.in);
+        while(in.hasNextInt()) {
+            int num = in.nextInt();
+            if (num < 0) 
+                break;
+            arrayList.add(new Integer(num));
+        }
+        int new_length = processArray(arrayList);
+        for(int i=0; i<new_length; i++)
+            System.out.println(arrayList.get(i));
+    }
+}
